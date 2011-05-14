3a0be1d5047d3e1136c4761cfc3de9306ea5f4a0
Alec Hartman <alechartman@Alec-Hartmans-MacBook-Pro.local>
2011-05-14 00:05:33 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Giftnetwork::Application.config.secret_token = '50f38467ca8ddb7e6a28a5d52a08e8aa8e251fb093fa01f90880dc1088c9cee1dceb182eb29cbdb5b7561c7eb3af3837f35cb66079adf00b9315a2fb8c743770'
