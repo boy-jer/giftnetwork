9cb4d442e421c5a2e0701c800fdad7daac0283d9
Alec Hartman <alechartman@Alec-Hartmans-MacBook-Pro.local>
2011-05-18 15:31:16 -0400

    deploy setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,21 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require 'bundler/capistrano'
+set :application, "giftnetwork"
+set :repository,  "git@github.com:alechartman/giftnetwork.git"
+set :user, "railsapps"
+set :use_sudo, false
+set :deploy_to, "/home/railsapps/public_html/#{application}"
 
-set :scm, :subversion
+set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+role :web, "giftnetwork.nyconrails.com"                          # Your HTTP server, Apache/etc
+role :app, "giftnetwork.nyconrails.com"                          # This may be the same as your `Web` server
+role :db,  "giftnetwork.nyconrails.com", :primary => true # This is where Rails migrations will run
 
 # If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
 # namespace :deploy do
 #   task :start do ; end
 #   task :stop do ; end
@@ -20,3 +23,20 @@ role :db,  "your slave db-server here"
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
 # end
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+#RVM stuff from http://rvm.beginrescueend.com/integration/capistrano/
+set :default_environment, {
+  'PATH' => "/home/railsapps/.rvm/gems/ruby-1.9.2-p136/bin:/home/railsapps/.rvm/bin:/home/railsapps/.rvm/rubies/ruby-1.9.2-p136/bin:$PATH",
+  'RUBY_VERSION' => 'ruby 1.9.2',
+  'GEM_HOME'     => '/home/railsapps/.rvm/gems/ruby-1.9.2-p136/',
+  'GEM_PATH'     => '/home/railsapps/.rvm/gems/ruby-1.9.2-p136/',
+  'BUNDLE_PATH'  => '/home/railsapps/.rvm/gems/ruby-1.9.2-p136/'  # If you are using bundler.
+}
