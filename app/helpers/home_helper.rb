df1db37c8d8a9646762826311962e98e54477438
Alec Hartman <alechartman@Alec-Hartmans-Office-iMac.local>
2011-05-24 18:41:16 -0400

    deploying amazon images and search fix

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,14 @@
 module HomeHelper
+  def getAmazonImage(asin)
+    image_request = Sucker.new( :locale => :us, :key => 'AKIAJY7TE7TS3ZZVIIDA', :secret => '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')
+    image_request << {'Operation' => 'ItemLookup', 'IdType' => 'ASIN', 'ItemId' => asin, 'ResponseGroup' => 'Images'}
+    image_response = image_request.get
+    image_response.each('Item') do |image|
+      if image['SmallImage'] != nil
+        return image['SmallImage']['URL']
+      else
+        return false
+      end
+    end
+  end
 end
