55b8f8f514c35462c1ec37cdcdbc05b320d05212
Alec Hartman <alechartman@Alec-Hartmans-Office-iMac.local>
2011-06-27 18:10:43 -0400

    updating

--- app/models/daily_deal.rb
+++ app/models/daily_deal.rb
@@ -1,2 +1,3 @@
 class DailyDeal < ActiveRecord::Base
+  has_many :daily_deal_locations
 end
--- app/models/daily_deal_location.rb
+++ app/models/daily_deal_location.rb
@@ -1,7 +1,11 @@
 class DailyDealLocation < ActiveRecord::Base
   acts_as_gmappable
+  belongs_to :daily_deal
   def gmaps4rails_address
     #describe how to retrieve the address from your model, if you use directly a db column, you can dry your code, see wiki
     "#{self.address}, #{self.city}, #{self.state}, #{self.zip}"
+  end
+  def gmaps4rails_infowindow
+    "<img src='#{self.daily_deal.small_image}' align='left' style='max-height:100px;margin-right:15px;' /><b>#{self.daily_deal.yipit_title}</b><br>Found through: #{self.daily_deal.source_name}"
   end
 end
